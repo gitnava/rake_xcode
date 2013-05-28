25582cb183c54648a154ad334665e52bd15c5037
Brian Tanner <btanner@edmunds.com>
2013-05-28 11:54:54 -0700

    wip

--- lib/rake_xcode.rb
+++ lib/rake_xcode.rb
@@ -11,7 +11,7 @@ module RakeXcode
   end
 
   class Xcode
-    attr_accessor :workspace, :configuration, :target, :scheme, :arch, :sdk, :profile, :identity, :testflight, :build_dir, :build_number, :version, :marketing_version
+    attr_accessor :workspace, :organization, :organization_id, :bundle_id, :configuration, :target, :source_dir, :scheme, :arch, :sdk, :profile, :identity, :testflight, :build_dir, :build_number, :version, :marketing_version
 
     def scheme_dir
       "#@configuration-#@sdk"
@@ -55,8 +55,8 @@ module RakeXcode
 
     def build(actions)
       roots = "#{root_dir}/#@build_dir/#@workspace/Build/Products"
-      sh "xcodebuild -workspace '#@workspace.xcworkspace' -configuration '#@configuration' -scheme '#@scheme' #{actions.join(' ')} SYMROOT=#{roots} OBJROOT=#{roots}"
-    end
+      sh "xctool -workspace '#@workspace.xcworkspace' -configuration '#@configuration' -scheme '#@scheme' #{actions.join(' ')} SYMROOT=#{roots} OBJROOT=#{roots}"
+    end      
 
     def package
       sh "xcrun -sdk #@sdk PackageApplication -v #{app_path} --sign '#@identity' --embed '#@profile' -o '#{ipa_path}'"
@@ -101,13 +101,16 @@ module RakeXcode
     yield @xcode
     
     desc "Downloads ruby gem build dependencies from Bundler Gemfile"
-    Rake::Task.define_task 'bundle' do
+    task :bundle => ['Gemfile.lock']
+
+    file 'Gemfile.lock' => ['Gemfile'] do
       sh "chmod a+w Gemfile.lock"
       sh "bundle update"
     end    
 
     desc "Downloads and constructs Cocoapods dependency project"
-    Rake::Task.define_task 'pods' => ['bundle'] do
+    task :pod => [:bundle]
+    file 'Podfile.lock'  => ['Podfile'] do
       sh "chmod a+w #{@xcode.workspace}.xcworkspace/contents.xcworkspacedata"
       sh "chmod -R a+w Pods" if File.exist? 'Pods'
       sh "chmod a+w Podfile.lock"
@@ -115,13 +118,13 @@ module RakeXcode
     end
 
     desc "Clean build output for #{@xcode.scheme_dir}"
-    task 'clean' do
+    task :clean do
       @xcode.build(['clean'])
       sh "rm -rf #{@xcode.output_path}"
     end
 
-    desc "Run xcodebuild for #{@xcode.scheme_dir}"
-    task 'build' => ['pods'] do
+    desc "Build scheme #{@xcode.scheme_dir} with xctool"
+    task :build => [:pod] do
       sh "find . -name '*-Info.plist' -exec chmod a+w {} \\;"
       sh "find . -name 'project.pbxproj' -exec chmod a+w {} \\;"  
       sh "security unlock-keychain -p #{ENV['XKEYPASS']} ~/Library/Keychains/login.keychain" if ENV['XKEYPASS']
@@ -129,35 +132,58 @@ module RakeXcode
       sh "xcrun agvtool new-version -all #{@xcode.build_number ? @xcode.build_number : @xcode.version}"
       @xcode.build(['build'])
     end
+    
+    desc "Build & test scheme #{@xcode.scheme_dir} with xctool"
+    task :test => [:pod] do
+      sh "find . -name '*-Info.plist' -exec chmod a+w {} \\;"
+      sh "find . -name 'project.pbxproj' -exec chmod a+w {} \\;"  
+      sh "security unlock-keychain -p #{ENV['XKEYPASS']} ~/Library/Keychains/login.keychain" if ENV['XKEYPASS']
+      sh "xcrun agvtool new-marketing-version  #{@xcode.marketing_version ? @xcode.marketing_version : @xcode.version}"
+      sh "xcrun agvtool new-version -all #{@xcode.build_number ? @xcode.build_number : @xcode.version}"
+      @xcode.build(['test'])
+    end    
+    
+    desc "Generate appledoc for #{@xcode.source_dir}"
+    task :appledoc do
+      sh "appledoc --project-name \"#{@xcode.target}\" --project-company \"#{@xcode.organization}\"" +
+        " --company-id #{@xcode.organization_id} --ignore \".m\" --explicit-crossref --keep-undocumented-objects" +
+        " --keep-undocumented-members --no-repeat-first-par --no-warn-missing-arg --no-warn-undocumented-object" +
+        " --no-warn-undocumented-member --no-warn-empty-description --docset-bundle-id \"#{@xcode.bundle_id}\" " +
+        " --docset-bundle-name \"#{@xcode.target}\" --keep-intermediate-files --output Docs/ #{@xcode.source_dir}"
+    end
 
     desc "Package app as an IPA file for #{@xcode.scheme_dir}"
-    Rake::Task.define_task 'package:ipa' =>['build'] do
+    task 'package:ipa' =>[:build] do
       @xcode.package()
     end
 
-    desc "Build frankied app bundle"
-    task 'frank:build' => ['pods'] do
-      sh "frank build --workspace='#{@xcode.workspace}.xcworkspace' --scheme='#{@xcode.scheme}' --arch=i386"
+    namespace :frank do
+      desc "Build frankied app bundle"
+      task :build => [:pod] do
+        sh "frank build --workspace='#{@xcode.workspace}.xcworkspace' --scheme='#{@xcode.scheme}' --arch=i386"
+      end
     end
 
-    if @xcode.testflight
-      desc 'Uploads ipa and zipped dsym files to TestFlight'
-      task 'upload:testflight' do
-        raise 'Missing IPA file, run package:ipa first!' if !File.exists? @xcode.ipa_path
-        File.delete @xcode.dsym_zip_path if File.exists? @xcode.dsym_zip_path
-        chdir @xcode.output_path do
-          sh "zip -r #{@xcode.dsym_zip_file} #{@xcode.dsym_dir}"
+    namespace :upload do
+      if @xcode.testflight
+        desc 'Uploads ipa and zipped dsym files to TestFlight'
+        task :testflight do
+          raise 'Missing IPA file, run package:ipa first!' if !File.exists? @xcode.ipa_path
+          File.delete @xcode.dsym_zip_path if File.exists? @xcode.dsym_zip_path
+          chdir @xcode.output_path do
+            sh "zip -r #{@xcode.dsym_zip_file} #{@xcode.dsym_dir}"
+          end
+          command = "curl 'http://testflightapp.com/api/builds.json' -i "
+          command << "-F file=@#{@xcode.ipa_path} "
+          command << "-F dsym=@#{@xcode.dsym_zip_path} "
+          command << "-F api_token='#{@xcode.testflight.api_token}' "
+          command << "-F team_token='#{@xcode.testflight.team_token}' "
+          command << "-F notes='#{@xcode.testflight.notes}' "
+          command << "-F notify='#{!@xcode.testflight.notify}' "
+          command <<  "-F distribution_lists='#{@xcode.testflight.distribution_lists.join(', ')}'" unless @xcode.testflight.distribution_lists.empty?
+  #        puts "empty #{@xcode.testflight.distribution_lists.empty?} command #{command}"
+          sh command
         end
-        command = "curl 'http://testflightapp.com/api/builds.json' -i "
-        command << "-F file=@#{@xcode.ipa_path} "
-        command << "-F dsym=@#{@xcode.dsym_zip_path} "
-        command << "-F api_token='#{@xcode.testflight.api_token}' "
-        command << "-F team_token='#{@xcode.testflight.team_token}' "
-        command << "-F notes='#{@xcode.testflight.notes}' "
-        command << "-F notify='#{!@xcode.testflight.notify}' "
-        command <<  "-F distribution_lists='#{@xcode.testflight.distribution_lists.join(', ')}'" unless @xcode.testflight.distribution_lists.empty?
-#        puts "empty #{@xcode.testflight.distribution_lists.empty?} command #{command}"
-        sh command
       end
     end
 
